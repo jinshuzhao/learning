2b57ddb74d60705a0d2d088bdad4f83689aa677e
jinshuzhao <1018772733@qq.com>
2016-04-16 23:06:15 +0800

    dsda

--- python.py
+++ python.py
@@ -1,7 +1,3 @@
 #!/usr/bin/env python
 print "Hello World"
-<<<<<<< HEAD
 print "Hello World Hello World"
-=======
-print "Hello Hello Hello Hello Hello"
->>>>>>> 6cd9330c48a45c0d9a3fc5048a3041c1ba37ff43
